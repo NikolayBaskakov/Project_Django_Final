9ed99ee647e8ab0d6e04121dfe6200816289bbf0
Nikolay Baskakov <bnn300702@yandex.ru>
2024-03-08 15:19:45 +0300

    f

--- project/app/views.py
+++ project/app/views.py
@@ -4,16 +4,25 @@ from .forms import UploadFileForm
 
 # Create your views here.
 def upload_file(request):
-    
     def handle(file, word):
             words = open(file).read()
             return words.count(word)
         
-    if request.method == 'POST':
+    def handle_uploaded_file(f, request_word):
+        with open(f"C:/Users/bnn30/Downloads/uploads/{f.name}", "wb+") as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+                
+        return handle(f"C:/Users/bnn30/Downloads/uploads/{f.name}", request_word)
+                
+    
+        
+    if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            r = handle(request.FILES['file'], request.word)
-            return HttpResponse(f'{r}')
+            res = handle_uploaded_file(form.cleaned_data['file'], request.POST.get('word'))
+            return render(request, 'index.html', {'count':res})
     else:
         form = UploadFileForm()
+        
     return render(request, 'form.html', {'form': form})
